a1af77c9ba29c05f0216de74f8d854cd9cf0d483
Sidik <sidikppln@gmail.com>
2021-10-13 14:07:29 +0700

    model

--- application/models/Contoh_model.php
+++ application/models/Contoh_model.php
@@ -3,12 +3,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Contoh_model extends CI_Model {
 
+    protected $_table = 'data_contoh';
+
 public function get()
 {
-    return $this->db->get('data_contoh')->result_array();
-    
-
+    return $this->db->get($this->_table)->result_array();
+}
+public function getDetail($id)
+{
+    return $this->db->get_where($this->_table,['id'=> $id])->row_array();
 
 }
-
+public function update($id)
+{
+    $this->db->update($this->_table,['id'=> $id]);
+    return $this->db->affected_rows();
+}
+public function delete($id)
+{
+    $this->db->delete($this->_table,['id'=> $id]);
+    return $this->db->affected_rows();
+}
+public function create($data)
+{
+    $this->db->insert($this->_table, $data);
+    return $this->db->affected_rows();
+}
+public function createBatch($data)
+{
+    $this->db->insert_batch($this->_table, $data);
+    return $this->db->affected_rows();
+}
+public function count()
+{
+    return $this->db->get($this->_table)->num_rows();
+}
 }
